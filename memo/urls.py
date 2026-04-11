97abb0c81a1d765f810301843982142f608f8721
Kosseno <emncrft01@hotmail.com>
2026-04-10 23:20:25 -0600

    docs: added comments to urls.py
    comentarios añadidos a urls

--- memo/urls.py
+++ memo/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('juego.urls')),
-    path('api/', include('api.urls')),
+    path('', include('juego.urls')),  # Incluye la URL de la app juego
+    path('api/', include('api.urls')),  # Incluye la URL de la app API
 ]
